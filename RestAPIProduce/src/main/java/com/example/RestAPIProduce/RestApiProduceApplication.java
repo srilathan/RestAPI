b312369147b792eb928f71ed3dc653f3772c37be
IN_MOURITECH\srilathan <srilathan.n@mouritech.com>
2021-10-04 06:46:47 +0530

    RestAPIProducer

---
+++ RestAPIProduce/src/main/java/com/example/RestAPIProduce/RestApiProduceApplication.java
@@ -0,0 +1,13 @@
+package com.example.RestAPIProduce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiProduceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiProduceApplication.class, args);
+	}
+
+}
---
+++ RestAPIProduce/src/test/java/com/example/RestAPIProduce/RestApiProduceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RestAPIProduce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiProduceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
